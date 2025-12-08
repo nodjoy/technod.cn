61c8f15db654ba45e035c8367ef039a06534125c
nod <nodjoy@163.com>
2025-12-09 00:36:35 +0800

    .

---
+++ status/config.js
@@ -0,0 +1,37 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '网站状态监控 - TechNod',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    // 'm784488775-dd1ad84b209c05f8e185c33e',
+    // 'm784490063-7b5da437e7f1e0d67613714d',
+    // 'm784497419-de55aa09902ccb3ab22d548a',
+    // 'm784496436-71a4bf7b1e3bdf7756be131b',
+    'u2238333-58888f34e3a87c275ba88766',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '返回首页',
+      url: 'https://technod.cn'
+    },
+    // {
+    //   text: 'GitHub',
+    //   url: 'https://github.com/yb/uptime-status'
+    // },
+    // {
+    //   text: 'Blog',
+    //   url: 'https://abo.xyz/'
+    // },
+  ],
+};
